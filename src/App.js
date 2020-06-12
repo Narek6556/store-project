f466aed1c660db366f64d1d6e898456d7469f424
Narek Aghasyan <aghasyan.na@gmail.com>
2020-06-12 23:48:00 +0400

    27:54_added_some_styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FaHome,FaApplePay } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-6">column number one <FaHome size = {30} color = 'Green'/></div>
+          <div className="col-6">column number two <FaApplePay size = {45} color = 'Blue'/></div>
+        </div>
+      </div>
     </div>
   );
 }
